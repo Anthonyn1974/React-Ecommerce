0eff6a885e788935c8f6a4029865f4d8f1e2a441
Anthony Nicholls <Anthony.Nicholls@gmail.com>
2020-02-23 15:10:53 +0000

    broken home page into components

--- src/Pages/Homepage/homepage.component.jsx
+++ src/Pages/Homepage/homepage.component.jsx
@@ -1,37 +1,10 @@
 import React from "react";
 import "./homepage.style.scss";
+import Directory from "../../Components/Directory/directory.component";
 
 const Homepage = () => (
   <div className="homepage">
-    <div className="directory-menu">
-      <div className="menu-item">
-        <div className="content">
-          <h1 className="title">HATS</h1>
-          <span className="subtitle">SHOP NOW</span>
-        </div>
-      </div>
-
-      <div className="menu-item">
-        <div className="content">
-          <h1 className="title">HATS</h1>
-          <span className="subtitle">SHOP NOW</span>
-        </div>
-      </div>
-
-      <div className="menu-item">
-        <div className="content">
-          <h1 className="title">HATS</h1>
-          <span className="subtitle">SHOP NOW</span>
-        </div>
-      </div>
-
-      <div className="menu-item">
-        <div className="content">
-          <h1 className="title">HATS</h1>
-          <span className="subtitle">SHOP NOW</span>
-        </div>
-      </div>
-    </div>
+    <Directory />
   </div>
 );
 
